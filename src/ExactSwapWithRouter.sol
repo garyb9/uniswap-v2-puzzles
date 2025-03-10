8c2f37cfab674fbb2496df86a11558fd8bd0d856
garyb9 <gberkinfand@gmail.com>
2025-03-10 19:41:35 +0200

    exact swap with router

--- src/ExactSwapWithRouter.sol
+++ src/ExactSwapWithRouter.sol
@@ -19,6 +19,26 @@ contract ExactSwapWithRouter {
 
     function performExactSwapWithRouter(address weth, address usdc, uint256 deadline) public {
         // your code start here
+
+        IUniswapV2Router routerInterface = IUniswapV2Router(router);
+        uint256 usdcAmount = 1337e6;
+        uint256 wethBalance = IERC20(weth).balanceOf(address(this));
+
+        address[] memory path = new address[](2);
+        path[0] = weth; // tokenIn;
+        path[1] = usdc; // tokenOut;
+
+        IERC20(weth).approve(router, wethBalance);
+        routerInterface.swapExactTokensForTokens(wethBalance, usdcAmount, path, address(this), deadline);
+
+        uint256 usdcBalance = IERC20(usdc).balanceOf(address(this));
+
+        address[] memory path2 = new address[](2);
+        path2[0] = usdc; // tokenIn;
+        path2[1] = weth; // tokenOut;
+
+        IERC20(usdc).approve(router, usdcBalance - usdcAmount);
+        IUniswapV2Router(router).swapExactTokensForTokens(usdcBalance - usdcAmount, 1, path2, address(this), deadline);
     }
 }
 
